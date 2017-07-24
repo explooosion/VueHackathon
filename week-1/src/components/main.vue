540ab435c7368ac9c8e1e97719cb24cb20516e1f
Robby <ta7382@gmail.com>
2017-07-24 19:54:03 +0800

    remove static  and move to asstes

--- week-1/src/components/main.vue
+++ week-1/src/components/main.vue
@@ -26,3 +26,33 @@
         </div>
     </div>
 </template>
+
+<script>
+
+
+let list = [
+    {
+        title: 'Heading',
+        info: 'Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. ',
+        btntext: 'View details »'
+    },
+    {
+        title: 'Heading',
+        info: 'Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. ',
+        btntext: 'View details »'
+    },
+    {
+        title: 'Heading',
+        info: 'Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.',
+        btntext: 'View details »'
+    },
+];
+
+export default {
+    data() {
+        return {
+            list: list
+        }
+    }
+}
+</script>
